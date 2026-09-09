47e7ed1a514f5d12f7155e03ae585efe3d833651
Arvin Ha <arvinha@usc.edu>
2026-09-09 06:38:15 +0000

    Finished split.cpp

--- split.cpp
+++ split.cpp
@@ -14,10 +14,39 @@ the function below should be the only one in this file.
 
 /* Add a prototype for a helper function here if you need */
 
+void append(Node*& head, Node* node);// Declare the function to append the list
+
 void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
 // WRITE YOUR CODE HERE
+  // Base case: empty list
+  if(in == nullptr) {
+      return;
+  }
+
+  Node* curr = in;
+  in = in->next;     // detach first nodefrom input list
+  curr->next = nullptr;
+
+  // Place curr into odds or evens
+  if(curr->value % 2 == 0) {
+      append(evens, curr);
+  }
+  else {
+      append(odds, curr);
+  }
+
+  // Iterate on the rest
+  split(in, odds, evens);
 }
 
 /* If you needed a helper function, write it here */
+void append(Node*& head, Node* node)
+{
+    if(head == nullptr) {
+        head = node;
+        return;
+    }
+    append(head->next, node);
+}
